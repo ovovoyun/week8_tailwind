5007a0f236428675d47578f03063a25318e9edc2
ovovoyun <136236394+ovovoyun@users.noreply.github.com>
2023-08-31 12:32:56 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ function moveOutputPlugin() {
 
 export default defineConfig({
   //Github Repo Name
-  base: '/week8/',
+  base: '/week8_tailwind/',
   plugins: [
     //指定檔案儲存後，自動重新整理
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
